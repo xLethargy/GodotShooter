d60622ddc8615b380f1ce94637dd8e2f5923cb7d
lethargy <qaimk@rocketmail.com>
2024-01-04 10:08:53 +0000

    Collision fixes
    Bug fixes on collisions where primary and secondary would clip through items. This is fixed - primary has been changed to a rigidbody but now has an area as well

--- Scenes/Scripts/enemy.gd
+++ Scenes/Scripts/enemy.gd
@@ -10,15 +10,13 @@ func _ready():
 	current_position = self.position
 
 func hit():
-	print ("enemy hit")
+	print (current_health)
 	current_health -= 1
 	if current_health == 0:
 		self.position = Vector3(100, 100, 100)
-		$".".visible = false
 		$RespawnTestTimer.start()
 
 # TESTING
 func _on_respawn_test_timer_timeout():
 	self.position = current_position
-	self.visible = true
 	current_health = max_health
--- Scenes/Scripts/primary.gd
+++ Scenes/Scripts/primary.gd
@@ -1,22 +1,26 @@
-extends Area3D
+extends RigidBody3D
 
 @export var speed: float = 50
 
 func _process(delta):
 	var direction_facing: Vector3 = get_global_transform().basis.z
-	position += direction_facing * speed * delta
+	#position += direction_facing * speed * delta
+	
+	linear_velocity = direction_facing * 50
 
-func _on_body_entered(body):
-	if "hit" in body:
-		body.hit()
-		queue_free()
-	
-	
-	queue_free()
-	
+
 
 func _on_enemy_hit():
 	pass
 
 func _on_destroy_timer_timeout():
+	queue_free()
+
+
+func _on_collision_detector_body_entered(body):
+	if "hit" in body:
+		body.hit()
+		queue_free()
+	
+	
 	queue_free()
